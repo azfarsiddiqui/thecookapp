aff00bb298925993596d11abb7dd4a306d424459
Jeff Tan-Ang <jeff@appsperhaps.com>
2013-02-22 16:23:48 +1100

    Added header to activity screen

---
+++ Cook/Classes/Controllers/ActivityHeaderViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ActivityHeaderViewCell.h
+//  Cook
+//
+//  Created by Jeff Tan-Ang on 22/02/13.
+//  Copyright (c) 2013 Cook Apps Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityHeaderViewCell : UICollectionReusableView
+
+@end
