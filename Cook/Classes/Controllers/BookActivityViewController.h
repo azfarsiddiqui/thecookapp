7ed0b055225fa6221e9f9f7f9f922e54cfed0713
Jeff Tan-Ang <jeff@appsperhaps.com>
2013-02-28 14:32:59 +1100

    Added paging between contents and activity

---
+++ Cook/Classes/Controllers/BookActivityViewController.h
@@ -0,0 +1,17 @@
+//
+//  BookActivityViewController.h
+//  Cook
+//
+//  Created by Jeff Tan-Ang on 28/02/13.
+//  Copyright (c) 2013 Cook Apps Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CKBook;
+
+@interface BookActivityViewController : UICollectionViewController
+
+- (id)initWithBook:(CKBook *)book;
+
+@end
