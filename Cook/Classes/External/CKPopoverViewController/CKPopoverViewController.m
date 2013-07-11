82cd496ac2620ff5bc99a0f4570a78a3226d704e
Jeff Tan-Ang <jeff@appsperhaps.com>
2013-07-11 11:47:08 +1000

    Tweaked popover

--- Cook/Classes/External/CKPopoverViewController/CKPopoverViewController.m
+++ Cook/Classes/External/CKPopoverViewController/CKPopoverViewController.m
@@ -11,6 +11,7 @@
 @interface CKPopoverViewController () <UIGestureRecognizerDelegate>
 
 @property (nonatomic, strong) UIViewController *contentViewController;
+@property (nonatomic, assign) CKPopoverViewControllerDirection direction;
 @property (nonatomic, assign) CGPoint anchorOffset;
 @property (nonatomic, assign) id<CKPopoverViewControllerDelegate> delegate;
 @property (nonatomic, strong) UIView *popoverContainerView;
@@ -22,12 +23,13 @@
 @implementation CKPopoverViewController
 
 #define kOverlayViewAlpha           0.5
-#define kOverlayViewFadeInDuration  0.2
-#define kOverlayViewFadeOutDuration 0.2
-#define kPopoverFadeInDuration      0.3
+#define kOverlayViewFadeInDuration  0.3
+#define kOverlayViewFadeOutDuration 0.3
+#define kPopoverFadeInDuration      0.2
 #define kPopoverFadeInDelay         0.0
-#define kPopoverFadeOutDuration     0.3
+#define kPopoverFadeOutDuration     0.2
 #define kPopoverFadeOutDelay        0.0
+#define kPopoverShiftOffset         10.0
 
 - (id)initWithContentViewController:(UIViewController *)contentViewController
                            delegate:(id<CKPopoverViewControllerDelegate>)delegate {
@@ -50,18 +52,19 @@
 }
 
 - (void)showInView:(UIView *)view direction:(CKPopoverViewControllerDirection)direction atPoint:(CGPoint)point {
+    self.direction = direction;
     self.parentView = view;
     self.view.frame = self.parentView.bounds;
     [self.parentView addSubview:self.view];
     
     // Inform delegate that we're about to appear and schedule overlayView to come in 0.1s in.
     [self.delegate popoverViewController:self willAppear:YES];
-    [self showOverlay:YES delay:0.1 completion:^{}];
     
     // Prepare contentView within the popover.
     UIEdgeInsets contentInsets = [self contentInsetsForDirection:direction];
     UIView *contentView = self.contentViewController.view;
     UIImageView *popoverContainerView = [[UIImageView alloc] initWithImage:[self popoverBackgroundImageForDirection:direction]];
+    popoverContainerView.backgroundColor = [UIColor clearColor];
     popoverContainerView.userInteractionEnabled = YES;
     popoverContainerView.frame = [self popoverContainerFrameForDirection:direction point:point];
     contentView.frame = CGRectMake(contentInsets.left,
@@ -69,7 +72,8 @@
                                    contentView.frame.size.width,
                                    contentView.frame.size.height);
     [popoverContainerView addSubview:contentView];
-    //popoverContainerView.alpha = 0.0;
+    popoverContainerView.transform = [self popoverContainerTransformForDirection:direction show:NO];
+    popoverContainerView.alpha = 0.0;
     [self.view addSubview:popoverContainerView];
     self.popoverContainerView = popoverContainerView;
     
@@ -77,6 +81,8 @@
                           delay:kPopoverFadeInDelay
                         options:UIViewAnimationOptionCurveEaseIn
                      animations:^{
+                         [self showOverlay:YES animated:NO];
+                         popoverContainerView.transform = [self popoverContainerTransformForDirection:direction show:YES];
                          popoverContainerView.alpha = 1.0;
                      }
                      completion:^(BOOL finished) {
@@ -90,20 +96,35 @@
 
 - (void)hide {
     
-    [self showOverlay:NO delay:0.0 completion:^{
-        
-        // Remove from superview.
-        [self.view removeFromSuperview];
-        [self.delegate popoverViewController:self didAppear:NO];
-        
-    }];
+    [UIView animateWithDuration:kPopoverFadeOutDuration
+                          delay:0.0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^{
+                         [self showOverlay:NO animated:NO];
+                         self.popoverContainerView.transform = [self popoverContainerTransformForDirection:self.direction show:NO];
+                         self.popoverContainerView.alpha = 0.0;
+                     }
+                     completion:^(BOOL finished) {
+                         
+                         // Remove from superview.
+                         [self.view removeFromSuperview];
+                         [self.delegate popoverViewController:self didAppear:NO];
+                     }];
 }
 
 #pragma mark - UIGestureRecognizerDelegate methods
 
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
     CGPoint tapPoint = [touch locationInView:self.view];
-    return !CGRectContainsPoint(self.popoverContainerView.frame, tapPoint);
+    UIEdgeInsets insets = [self popoverBackgroundImageInsetsForDirection:self.direction];
+    CGRect popoverFrame = (CGRect){
+        self.popoverContainerView.frame.origin.x + insets.left,
+        self.popoverContainerView.frame.origin.y + insets.top,
+        self.popoverContainerView.frame.size.width - insets.left - insets.right,
+        self.popoverContainerView.frame.size.height - insets.top - insets.bottom
+    };
+    
+    return !CGRectContainsPoint(popoverFrame, tapPoint);
 }
 
 #pragma mark - Private methods
@@ -137,6 +158,42 @@
                      }];
 }
 
+- (void)showOverlay:(BOOL)show animated:(BOOL)animated {
+    
+    if (show) {
+        UIView *overlayView = [[UIView alloc] initWithFrame:self.view.bounds];
+        overlayView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+        overlayView.backgroundColor = [UIColor blackColor];
+        overlayView.alpha = 0.0;
+        [self.view insertSubview:overlayView belowSubview:self.popoverContainerView];
+        self.overlayView = overlayView;
+    }
+    
+    if (animated) {
+        [UIView animateWithDuration:show ? kOverlayViewFadeInDuration : kOverlayViewFadeOutDuration
+                              delay:0.0
+                            options:show ? UIViewAnimationOptionCurveEaseOut : UIViewAnimationOptionCurveEaseIn
+                         animations:^{
+                             self.overlayView.alpha = show ? kOverlayViewAlpha : 0.0;
+                         }
+                         completion:^(BOOL finished) {
+                             if (!show) {
+                                 [self.overlayView removeFromSuperview];
+                                 self.overlayView = nil;
+                             }
+                         }];
+    } else {
+        if (show) {
+            self.overlayView.alpha = kOverlayViewAlpha;
+        } else {
+            [self.overlayView removeFromSuperview];
+            self.overlayView = nil;
+        }
+    }
+}
+
+
+
 - (void)overlayTapped:(UITapGestureRecognizer *)tapGesture {
     [self hide];
 }
@@ -156,6 +213,25 @@
             break;
     }
     return popoverImage;
+}
+
+- (UIEdgeInsets)popoverBackgroundImageInsetsForDirection:(CKPopoverViewControllerDirection)direction {
+    UIEdgeInsets insets = UIEdgeInsetsZero;
+    
+    switch (direction) {
+        case CKPopoverViewControllerTop:
+            insets = (UIEdgeInsets){ 39.0, 27.0, 39.0, 27.0 };
+            break;
+        case CKPopoverViewControllerLeft:
+            insets = (UIEdgeInsets){ 28.0, 41.0, 40.0, 41.0 };
+            break;
+        case CKPopoverViewControllerBottom:
+        case CKPopoverViewControllerRight:
+        default:
+            break;
+    }
+    
+    return insets;
 }
 
 - (CGRect)popoverContainerFrameForDirection:(CKPopoverViewControllerDirection)direction
@@ -193,10 +269,11 @@
         case CKPopoverViewControllerTop:
             insets = UIEdgeInsetsMake(46.0, 56.0, 59.0, 56.0);
         case CKPopoverViewControllerLeft:
+            insets = UIEdgeInsetsMake(46.0, 56.0, 59.0, 56.0);
+            break;
         case CKPopoverViewControllerBottom:
         case CKPopoverViewControllerRight:
         default:
-            insets = UIEdgeInsetsMake(46.0, 56.0, 59.0, 56.0);
             break;
     }
     return insets;
@@ -209,13 +286,36 @@
             anchorPoint = CGPointMake(-2.0, 30.0);
             break;
         case CKPopoverViewControllerLeft:
+            anchorPoint = CGPointMake(24.0, 58.0);
+            break;
         case CKPopoverViewControllerBottom:
         case CKPopoverViewControllerRight:
         default:
-            anchorPoint = CGPointMake(24.0, 58.0);
             break;
     }
     return anchorPoint;
+}
+
+- (CGAffineTransform)popoverContainerTransformForDirection:(CKPopoverViewControllerDirection)direction show:(BOOL)show {
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    if (show) {
+        return transform;
+    }
+    
+    switch (direction) {
+        case CKPopoverViewControllerTop:
+            transform = CGAffineTransformMakeTranslation(0.0, -kPopoverShiftOffset);
+            break;
+        case CKPopoverViewControllerLeft:
+            transform = CGAffineTransformMakeTranslation(-kPopoverShiftOffset, 00.0);
+            break;
+        case CKPopoverViewControllerBottom:
+        case CKPopoverViewControllerRight:
+        default:
+            break;
+    }
+    
+    return transform;
 }
 
 @end
