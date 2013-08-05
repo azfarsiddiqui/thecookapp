2c33d634147c5048c5a569317625499109ec56d0
Jeff Tan-Ang <jeff@appsperhaps.com>
2013-08-05 22:43:51 +1000

    Fixed unintended limitLabel on top left corner of screen.

--- Cook/Classes/External/CKEditViewController/CKTextEditViewController.m
+++ Cook/Classes/External/CKEditViewController/CKTextEditViewController.m
@@ -50,7 +50,7 @@
 #pragma mark - Lazy getters
 
 - (UILabel *)limitLabel {
-    if (!_limitLabel && self.textLimited) {
+    if (!_limitLabel && self.textLimited && [self showTitleLabel]) {
         
         _limitLabel = [[UILabel alloc] initWithFrame:CGRectZero];
         _limitLabel.backgroundColor = [UIColor clearColor];
