75d533a3344d4ca1e73b891de72e7e3011b84ea5
Jeff Tan-Ang <jeff@appsperhaps.com>
2013-08-11 00:18:06 +1000

    Fixed popover cancel button crashing on CKPhotoPickerVC

--- Cook/Classes/External/CKPhotoPickerViewController/CKPhotoPickerViewController.m
+++ Cook/Classes/External/CKPhotoPickerViewController/CKPhotoPickerViewController.m
@@ -77,6 +77,7 @@
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self.popoverViewController dismissPopoverAnimated:NO];
     [self.delegate photoPickerViewControllerCloseRequested];
 }
 
