cbc73a50433693f74471154e4e3a42c50ae9c667
Jeff Tan-Ang <jeff@appsperhaps.com>
2013-08-28 21:51:21 +1000

    Tweaked signin book

--- Cook/Classes/Controllers/Dashboard/SignUpBookCoverViewCell.m
+++ Cook/Classes/Controllers/Dashboard/SignUpBookCoverViewCell.m
@@ -13,6 +13,7 @@
 
 @interface SignUpBookCoverViewCell () <CKSignInButtonViewDelegate>
 
+@property (nonatomic, strong) UIImageView *dullOverlayView;
 @property (nonatomic, strong) CKSignInButtonView *registerButton;
 @property (nonatomic, strong) CKBlueSignInButtonView *signInButton;
 
@@ -20,18 +21,19 @@
 
 @implementation SignUpBookCoverViewCell
 
-#define kWidth  142.0
+#define kWidth  282.0   // 270 + 12 shadows left/right
 
 - (id)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
-        [self.contentView addSubview:self.signInButton];
+        [self.contentView addSubview:self.dullOverlayView];
         [self.contentView addSubview:self.registerButton];
+        [self.contentView addSubview:self.signInButton];
     }
     return self;
 }
 
 - (void)loadBook:(CKBook *)book {
-    self.bookCoverView.illustrationImageView.alpha = 0.5;
+//    self.bookCoverView.illustrationImageView.alpha = 0.5;
     [super loadBook:book];
 }
 
@@ -51,13 +53,20 @@
 
 #pragma mark - Properties
 
+- (UIImageView *)dullOverlayView {
+    if (!_dullOverlayView) {
+        _dullOverlayView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"cook_book_demo_overlay.png"]];
+        _dullOverlayView.center = self.bookCoverView.center;
+    }
+    return _dullOverlayView;
+}
+
 - (CKBlueSignInButtonView *)signInButton {
     if (!_signInButton) {
-        UIEdgeInsets insets = UIEdgeInsetsMake(0.0, 8.0, 8.0, 0.0);
         _signInButton = [[CKBlueSignInButtonView alloc] initWithWidth:self.registerButton.frame.size.width text:@"SIGN IN" activity:NO delegate:self];
         _signInButton.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleRightMargin;
-        _signInButton.frame = CGRectMake(insets.left,
-                                         self.contentView.bounds.size.height - _signInButton.frame.size.height - insets.bottom,
+        _signInButton.frame = CGRectMake(self.registerButton.frame.origin.x,
+                                         self.registerButton.frame.origin.y - _signInButton.frame.size.height + 8.0,
                                          _signInButton.frame.size.width,
                                          _signInButton.frame.size.height);
     }
@@ -66,10 +75,11 @@
 
 - (CKSignInButtonView *)registerButton {
     if (!_registerButton) {
+        UIEdgeInsets insets = UIEdgeInsetsMake(0.0, 0.0, 10.0, 0.0);
         _registerButton = [[CKSignInButtonView alloc] initWithWidth:kWidth text:@"REGISTER" activity:NO delegate:self];
         _registerButton.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleLeftMargin;
-        _registerButton.frame = CGRectMake(self.signInButton.frame.origin.x + self.signInButton.frame.size.width,
-                                           self.signInButton.frame.origin.y,
+        _registerButton.frame = CGRectMake(floorf((self.contentView.bounds.size.width - kWidth) / 2.0),
+                                           self.contentView.bounds.size.height - _registerButton.frame.size.height - insets.bottom,
                                            _registerButton.frame.size.width,
                                            _registerButton.frame.size.height);
     }
