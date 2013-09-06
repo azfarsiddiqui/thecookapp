c490475bc6a0b3e186f45f6946da67b42eb9c755
Gerald Kim <gerald@jtribe.com.au>
2013-09-06 23:41:03 +1000

    Added filters to CKPhotoPickerController. BookProfile uses square picker and recipe edit uses full image picker

--- Cook/Classes/External/CKPhotoPickerViewController/CKPhotoPickerViewController.h
+++ Cook/Classes/External/CKPhotoPickerViewController/CKPhotoPickerViewController.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSUInteger, CKPhotoPickerImageType) {
+    CKPhotoPickerImageTypeLandscape,
+    CKPhotoPickerImageTypeSquare
+};
+
 @protocol CKPhotoPickerViewControllerDelegate
 
 - (void)photoPickerViewControllerSelectedImage:(UIImage *)image;
@@ -17,7 +22,12 @@
 
 @interface CKPhotoPickerViewController : UIViewController
 
+@property (nonatomic, assign) CKPhotoPickerImageType type;
+
 - (id)initWithDelegate:(id<CKPhotoPickerViewControllerDelegate>)delegate;
+- (id)initWithDelegate:(id<CKPhotoPickerViewControllerDelegate>)delegate type:(CKPhotoPickerImageType)type;
 - (id)initWithDelegate:(id<CKPhotoPickerViewControllerDelegate>)delegate saveToPhotoAlbum:(BOOL)saveToPhotoAlbum;
+- (id)initWithDelegate:(id<CKPhotoPickerViewControllerDelegate>)delegate type:(CKPhotoPickerImageType)type
+      saveToPhotoAlbum:(BOOL)saveToPhotoAlbum showFilters:(BOOL)showFilters;
 
 @end
