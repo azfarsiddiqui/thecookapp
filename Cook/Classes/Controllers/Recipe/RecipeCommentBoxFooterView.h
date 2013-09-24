c8ddd085f1f20d7df9c60cd789999a0d9b7a4d9a
Jeff Tan-Ang <jeff@appsperhaps.com>
2013-09-24 12:25:58 +1000

    Updated blank profile icon

---
+++ Cook/Classes/Controllers/Recipe/RecipeCommentBoxFooterView.h
@@ -0,0 +1,13 @@
+//
+//  RecipeCommentBoxFooterView.h
+//  Cook
+//
+//  Created by Jeff Tan-Ang on 24/09/13.
+//  Copyright (c) 2013 Cook Apps Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeCommentBoxFooterView : UICollectionReusableView
+
+@end
---
+++ Cook/Classes/Controllers/Recipe/RecipeCommentBoxFooterView.m
@@ -0,0 +1,31 @@
+//
+//  RecipeCommentBoxFooterView.m
+//  Cook
+//
+//  Created by Jeff Tan-Ang on 24/09/13.
+//  Copyright (c) 2013 Cook Apps Pty Ltd. All rights reserved.
+//
+
+#import "RecipeCommentBoxFooterView.h"
+
+@implementation RecipeCommentBoxFooterView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
