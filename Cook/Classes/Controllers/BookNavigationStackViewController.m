0d89a5d451f60ca11b10e251f2b9f03fa9ebdf8e
Jeff Tan-Ang <jeff@appsperhaps.com>
2013-10-31 14:42:18 +1100

    Do not fade when sliding navbar in and out. Also slows down the sliding/fading transition speed.

--- Cook/Classes/Controllers/BookNavigationStackViewController.m
+++ Cook/Classes/Controllers/BookNavigationStackViewController.m
@@ -1658,22 +1658,26 @@
         
         if (show) {
             self.bookNavigationView.hidden = NO;
+            self.bookNavigationView.alpha = 0.0;
         }
         
         if (!slide) {
             self.bookNavigationView.transform = show ? CGAffineTransformIdentity : CGAffineTransformMakeTranslation(0.0, -self.bookNavigationView.frame.size.height);
+        } else {
+            self.bookNavigationView.alpha = 1.0;
         }
         
         // Inform status bar hide.
         [EventHelper postStatusBarHide:!show];
         
-        [UIView animateWithDuration:0.25
+        [UIView animateWithDuration:0.4
                               delay:0.0
-                            options:UIViewAnimationOptionCurveEaseIn
+                            options:UIViewAnimationOptionCurveEaseOut
                          animations:^{
-                             self.bookNavigationView.alpha = show ? 1.0 : 0.0;
                              if (slide) {
                                  self.bookNavigationView.transform = show ? CGAffineTransformIdentity : CGAffineTransformMakeTranslation(0.0, -self.bookNavigationView.frame.size.height);
+                             } else {
+                                 self.bookNavigationView.alpha = show ? 1.0 : 0.0;
                              }
                          }
                          completion:^(BOOL finished) {
