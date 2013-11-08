786cf6186d19673511aa563ab9ebbc191facec46
Jeff Tan-Ang <jeff@appsperhaps.com>
2013-11-08 23:56:59 +1100

    Fixed suggested books getting ticked with followed.

--- Cook/Classes/Controllers/Store/StoreBookViewController.m
+++ Cook/Classes/Controllers/Store/StoreBookViewController.m
@@ -167,8 +167,7 @@
 }
 
 - (void)bookSummaryViewUserFriendActioned {
-    self.updated = YES;
-    
+
     // Automatically close.
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
         [self closeTapped];
