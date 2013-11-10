d7d4419c33a091c07f68043dec2519d72ef8ce6b
Jeff Tan-Ang <jeff@appsperhaps.com>
2013-11-10 15:06:08 +1100

    Removed linking via FB if not registered with FB

--- Cook/Classes/Controllers/Store/FriendsStoreCollectionViewController.m
+++ Cook/Classes/Controllers/Store/FriendsStoreCollectionViewController.m
@@ -64,9 +64,9 @@
         return [NSIndexPath indexPathForItem:index inSection:0];
     }]];
     
-    if (self.books && ![[CKUser currentUser] isFacebookUser]) {
-        [insertIndexPaths addObject:[NSIndexPath indexPathForItem:[insertIndexPaths count] inSection:0]];
-    }
+//    if (self.books && ![[CKUser currentUser] isFacebookUser]) {
+//        [insertIndexPaths addObject:[NSIndexPath indexPathForItem:[insertIndexPaths count] inSection:0]];
+//    }
     
     [self.collectionView insertItemsAtIndexPaths:insertIndexPaths];
 }
@@ -97,11 +97,11 @@
 }
 
 - (void)showNoBooksCard {
-    CKUser *currentUser = [CKUser currentUser];
-    if ([currentUser isFacebookUser]) {
+//    CKUser *currentUser = [CKUser currentUser];
+//    if ([currentUser isFacebookUser]) {
         [[CardViewHelper sharedInstance] showCardText:@"NO FRIENDS" subtitle:@"USE SEARCH TO FIND PEOPLE YOU KNOW"
                                                  view:self.collectionView show:YES center:self.collectionView.center];
-    }
+//    }
 }
 
 - (void)errorInFriendFetchWithTitle:(NSString *)title message:(NSString *)message {
@@ -126,31 +126,31 @@
     NSInteger numItems = [super collectionView:collectionView numberOfItemsInSection:section];
     
     // Extra facebook button if not linked to facebook.
-    CKUser *currentUser = [CKUser currentUser];
-    if (self.dataLoaded && currentUser && ![currentUser isFacebookUser]) {
-        numItems += 1;
-    }
+//    CKUser *currentUser = [CKUser currentUser];
+//    if (self.dataLoaded && currentUser && ![currentUser isFacebookUser]) {
+//        numItems += 1;
+//    }
     
     return numItems;
 }
 
 #pragma mark - UICollectionViewDelegateFlowLayout methods
 
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout
-referenceSizeForHeaderInSection:(NSInteger)section {
-    
-    CGSize headerSize = CGSizeZero;
-    
-    CKUser *currentUser = [CKUser currentUser];
-    if (self.dataLoaded && ([self.books count] == 0) && ![currentUser isFacebookUser]) {
-        headerSize = (CGSize) {
-            floorf((collectionView.bounds.size.width - self.facebookButtonView.frame.size.width) / 2.0) - floorf(self.facebookButtonView.frame.size.width / 2.0) + 18,
-            0.0
-        };
-    }
-    
-    return headerSize;
-}
+//- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout
+//referenceSizeForHeaderInSection:(NSInteger)section {
+//    
+//    CGSize headerSize = CGSizeZero;
+//    
+//    CKUser *currentUser = [CKUser currentUser];
+//    if (self.dataLoaded && ([self.books count] == 0) && ![currentUser isFacebookUser]) {
+//        headerSize = (CGSize) {
+//            floorf((collectionView.bounds.size.width - self.facebookButtonView.frame.size.width) / 2.0) - floorf(self.facebookButtonView.frame.size.width / 2.0) + 18,
+//            0.0
+//        };
+//    }
+//    
+//    return headerSize;
+//}
 
 - (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
     if (indexPath.item == [self.books count]) {
