4a3802afd37971af26fbb57d01b7933aa18f00db
Gerald Kim <gerald@jtribe.com.au>
2013-11-25 08:10:08 +1100

    Center Tags label better

--- Cook/Classes/Controllers/Recipe/RecipeDetailsView.m
+++ Cook/Classes/Controllers/Recipe/RecipeDetailsView.m
@@ -611,20 +611,24 @@ typedef NS_ENUM(NSUInteger, EditPadDirection) {
 //            [self.tagsView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:tagsConstraintString options:0 metrics:metrics views:views]];
 //            [self.tagsView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[tagsLabel]-|" options:0 metrics:metrics views:views]];
 //        }
-        
+        UIImage *iconImage = [UIImage imageNamed:@"cook_customise_icon_tag"];
         UIEdgeInsets  defaultInsets = [CKEditingViewHelper contentInsetsForEditMode:NO];
         [self.editingHelper wrapEditingView:self.tagsLabel contentInsets:(UIEdgeInsets){
             defaultInsets.top + 8.0,
-            defaultInsets.left,
+            defaultInsets.left + 5.0,
             defaultInsets.bottom + 4.0,
-            defaultInsets.right - 2.0
-        } delegate:self white:YES iconImage:[UIImage imageNamed:@"cook_customise_icon_tag"]];
+            defaultInsets.right - 7.0 + iconImage.size.width //Add padding to make it look centered
+        } delegate:self white:YES iconImage:iconImage];
     }
     
     if ([self.recipeDetails.tags count] > 0 || self.editMode) {
         self.tagsLabel.alpha = 1.0;
         [self updateTagsFrame];
-        [self updateLayoutOffsetVertical:self.tagsLabel.frame.size.height + 25];
+        if (self.editMode) {
+            [self updateLayoutOffsetVertical:self.tagsLabel.frame.size.height + 15];
+        } else {
+            [self updateLayoutOffsetVertical:self.tagsLabel.frame.size.height + 10];
+        }
     } else {
         [self updateTagsFrame];
     }
@@ -665,16 +669,15 @@ typedef NS_ENUM(NSUInteger, EditPadDirection) {
     if (!self.editMode) {
         self.tagsLabel.frame = CGRectIntegral((CGRect){
             floorf((kWidth - size.width) / 2.0) > 0 ? floorf((kWidth - size.width) / 2.0) : 0,
-            self.layoutOffset.y + 20,
+            self.layoutOffset.y,
             size.width > kWidth ? kWidth : size.width,
             size.height
         });
     } else {
         //UGLY, autolayout this sucker to center properly later
-        UIImage *tagIconImage = [UIImage imageNamed:@"cook_customise_icon_tag"];
         self.tagsLabel.frame = CGRectIntegral((CGRect){
-            (floorf((kWidth - size.width) / 2.0) > 0 ? floorf((kWidth - size.width) / 2.0) : 0) + tagIconImage.size.width/2,
-            self.layoutOffset.y + 20,
+            (floorf((kWidth - size.width) / 2.0) > 0 ? floorf((kWidth - size.width) / 2.0) : 0),
+            self.layoutOffset.y + 15,
             size.width > kWidth ? kWidth : size.width,
             size.height
         });
