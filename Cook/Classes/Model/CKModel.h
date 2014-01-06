e66c1f186c16a9cd56b733a26bcb22cc5e29de3f
Jeff Tan-Ang <jeff@appsperhaps.com>
2014-01-06 23:14:12 +1100

    New typedef for Get that returns if it has cached result.

--- Cook/Classes/Model/CKModel.h
+++ Cook/Classes/Model/CKModel.h
@@ -13,6 +13,7 @@
 typedef void(^ObjectFailureBlock)(NSError *error);
 typedef void(^ObjectSuccessBlock)();
 typedef void(^GetObjectSuccessBlock)(id object);
+typedef void(^GetCachedObjectSuccessBlock)(id object, BOOL cached);
 typedef void(^NumObjectSuccessBlock)(int numObjects);
 typedef void(^BoolObjectSuccessBlock)(BOOL yesNo);
 typedef void(^ListObjectsSuccessBlock)(NSArray *results);
