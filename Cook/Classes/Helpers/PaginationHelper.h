58a44903a996b621e21149f36af0379cb45c2eb3
Jeff Tan-Ang <jeff@appsperhaps.com>
2014-05-19 11:20:20 +1000

    Updated updates screen.

---
+++ Cook/Classes/Helpers/PaginationHelper.h
@@ -0,0 +1,13 @@
+//
+//  PaginationHelper.h
+//  Cook
+//
+//  Created by Jeff Tan-Ang on 18/05/2014.
+//  Copyright (c) 2014 Cook Apps Pty Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PaginationHelper : NSObject
+
+@end
