e396f2e573afc1b21ff81c3c073daafd80b99c0c
Gerald Kim <gerald@jtribe.com.au>
2014-05-21 12:06:13 +1000

    set max Makes to be 60

--- Cook/Classes/Controllers/Recipe/ServesAndTimeEditViewController.m
+++ Cook/Classes/Controllers/Recipe/ServesAndTimeEditViewController.m
@@ -286,7 +286,7 @@
     if (!_makesSlider) {
         _makesSlider = [[TimeSliderView alloc] init];
         _makesSlider.minimumValue = 0.0;
-        _makesSlider.maximumValue = 38;
+        _makesSlider.maximumValue = 31;
         _makesSlider.minimumTrackTintColor = [UIColor colorWithRed:0.102 green:0.533 blue:0.961 alpha:1.000];
         _makesSlider.maximumTrackTintColor = [UIColor colorWithWhite:0.863 alpha:1.000];
         [_makesSlider setThumbImage:[UIImage imageNamed:@"cook_edit_serves_slider"] forState:UIControlStateNormal];
